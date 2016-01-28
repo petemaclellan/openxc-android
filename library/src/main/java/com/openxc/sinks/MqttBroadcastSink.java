d3b6c5ac0a9d1ad1f46c1788f29c1806c23ddb3c
petemaclellan <peter.maclellan@flextronics.com>
2016-01-27 18:02:46 -0800

    Refactored vehicle message pipeline so we only broadcast data when a value changes.  Also now we are sending all available vehicle data as a single JSON object

--- library/src/main/java/com/openxc/sinks/MqttBroadcastSink.java
+++ library/src/main/java/com/openxc/sinks/MqttBroadcastSink.java
@@ -1,14 +1,23 @@
 package com.openxc.sinks;
 
+import android.annotation.TargetApi;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 
 import com.openxc.messages.SimpleVehicleMessage;
 import com.openxc.messages.VehicleMessage;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by mlgpmacl on 1/27/2016.
@@ -17,11 +26,16 @@ public class MqttBroadcastSink extends ContextualVehicleDataSink {
     private BroadcasterThread mBroadcaster = new BroadcasterThread();
     private Context context;
     private String vehicleData = "";
+    private ConcurrentHashMap<String, String> currentVehicleStatus;
+    private Lock changeDetectLock = new ReentrantLock();
+    private Condition valueChanged = changeDetectLock.newCondition();
 
     public MqttBroadcastSink(Context context) {
         super(context);
         this.context = context;
+        currentVehicleStatus = new ConcurrentHashMap<>();
     }
+
     @Override
     public void stop() {
         mBroadcaster.done();
@@ -29,16 +43,47 @@ public class MqttBroadcastSink extends ContextualVehicleDataSink {
 
     @Override
     public void receive(VehicleMessage message) {
-        this.vehicleData = this.extractData(message);
-        mBroadcaster.run();
+        extractData(message);
+        //The rest is handled by BroadcasterThread
     }
 
-    protected String extractData(VehicleMessage message) {
-        String msg = "{\"d\":{";
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    protected void extractData(VehicleMessage message) {
+        //sketchy cast so we can extract fields
         SimpleVehicleMessage sv = (SimpleVehicleMessage) message;
-        msg += "\"" + sv.getName() + "\": " + sv.getValue().toString() + ", ";
-        msg += "}}";
-        return msg;
+        String key = sv.getName();
+        String newValue = sv.getValue().toString();
+
+        String result = currentVehicleStatus.put(key, newValue);
+        if (!Objects.equals(result, newValue)) {
+            //The value either changed or wasn't tracked previously
+            //Send the updated vehicle status set
+            try {
+                changeDetectLock.lock();
+                valueChanged.signal();
+            } finally {
+                changeDetectLock.unlock();
+            }
+        }
+    }
+
+    private void createJsonString() {
+        try {
+            changeDetectLock.lock();
+            //wait here until we get the signal that data has changed, or we timeout
+            valueChanged.await(30, TimeUnit.SECONDS);
+
+            String msg = "{\"d\":{";
+            for (Map.Entry<String, String> kv: currentVehicleStatus.entrySet()) {
+                msg += "\"" + kv.getKey() + "\": " + kv.getValue() + ", ";
+            }
+            msg += "}}";
+            vehicleData = msg;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            changeDetectLock.unlock();
+        }
     }
 
     private class BroadcasterThread extends Thread {
@@ -54,11 +99,16 @@ public class MqttBroadcastSink extends ContextualVehicleDataSink {
 
         @Override
         public void run() {
-            Intent intent = new Intent();
-            intent.setAction("com.pkg.perform.Ruby");
-            intent.putExtra("VehicleData", MqttBroadcastSink.this.vehicleData);
-            intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
-            context.sendBroadcast(intent);
+            while (mRunning) {
+                //this call will block until vehicle data changes and then set "vehicleData"
+                createJsonString();
+
+                Intent intent = new Intent();
+                intent.setAction("com.pkg.perform.Ruby");
+                intent.putExtra("VehicleData", vehicleData);
+                intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+                context.sendBroadcast(intent);
+            }
         }
     }
 }
